72751a82289fac7263f491bf862347c7df57a146
Mohammad Haddad <66420319+HaddadGit@users.noreply.github.com>
2020-06-27 23:42:20 +0430

    remove if statment in app.js and very shortest

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,25 +9,10 @@ function App() {
   function changeHandler(event) {
     const { name, value } = event.target;
     setContact(preValue => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: preValue.lName,
-          email: preValue.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: preValue.fName,
-          lName: value,
-          email: preValue.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: preValue.fName,
-          lName: preValue.lName,
-          email: value
-        };
-      }
+      return {
+        ...preValue,
+        [name]: value
+      };
     });
   }
   return (
